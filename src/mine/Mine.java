5704da6989ce72765a85e790de6f74aea4919326
berny11 <bernyalem@gmail.com>
2017-05-24 00:49:18 -0700

    Update Mine.java

--- src/mine/Mine.java
+++ src/mine/Mine.java
@@ -16,6 +16,9 @@ public class Mine {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hi every one");
+        System.out.println("hello world");
+        System.out.println("git practice");
     }
     
 }
